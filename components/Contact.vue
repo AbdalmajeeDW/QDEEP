74f4f6d94073057051740357b8f3c4309a697c18
AbdalmajeeDW <abd.almajed.alhashemi@gmail.com>
2024-04-16 15:03:08 +0300

    fix card

--- components/Contact.vue
+++ components/Contact.vue
@@ -2,17 +2,16 @@
   <div class="contact" id="contact">
     <div class="topSectionContact">
       <h2 class="titleSection">
-        Career at QDeep Russian
-        Research Institute
+        Career at QDeep Russian Research Institute
       </h2>
       <div class="text">
-        QDeep welcomes more and more students, PhDs and specialists to participate in future cutting-edge technical challenges.
+        QDeep welcomes more and more students,PhDs and specialists to participate in future cutting-edge technical challenges.
       </div>
     </div>
     <div class="BottomSectionContact">
       <div class="form">
         <div class="text">
-          Our vision and mission at QDeep are to bring about quantum advantage and play a pivotal role in the second quantum revolution.        </div>
+          Our vision and mission at QDeep are to bring about quantum advantage and play a pivotal role in the second quantum revolution.</div>
         
       </div>
     </div>
