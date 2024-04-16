a22de9f8c4e9834e5f6d77557473405fb899ad43
AbdalmajeeDW <abd.almajed.alhashemi@gmail.com>
2024-04-16 14:49:03 +0300

    finish site

--- components/footer.vue
+++ components/footer.vue
@@ -1,12 +1,12 @@
 <template>
 <div class="footer">
     ©2024 QDeep Technologies Co., Ltd.
-    <ul>
+    <!-- <ul>
     <li>Contacts</li>
     <li>Terms of Use</li>
     <li>Privacy Policy</li>
     <li>Privacy Policy</li>
     <li>Cookies</li>
-    </ul>
+    </ul> -->
 </div>
 </template>
